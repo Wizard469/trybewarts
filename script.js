5c450a3a361a1719eaf9c08f5f4a27a1d7d1844a
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-05-06 09:48:44 -0300

    Fixing ESlint/Stylelint issues

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const loginButton = document.getElementById('login-button');
 const password = document.getElementById('password');
 const email = document.getElementById('email');
 
-loginButton.addEventListener('click', function() {
+loginButton.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
